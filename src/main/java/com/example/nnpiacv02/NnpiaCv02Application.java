2f71157eb786c7daba11e528314d8beecbc2f5fc
st61034 <matej.patek.chot@seznam.cz>
2024-02-20 14:13:05 +0100

    init

---
+++ src/main/java/com/example/nnpiacv02/NnpiaCv02Application.java
@@ -0,0 +1,13 @@
+package com.example.nnpiacv02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NnpiaCv02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NnpiaCv02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nnpiacv02/NnpiaCv02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nnpiacv02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NnpiaCv02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
